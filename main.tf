162b0072d6a48291617db6a58f0a3acbcf6714e0
hajji-0081 <70376875+hajji-0081@users.noreply.github.com>
2021-03-26 00:47:31 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,13 +9,11 @@ terraform {
 provider "aws" {
   region = var.aws_region
   shared_credentials_file = "~/.aws/credentials"
-  profile                 = "nx_profile"
 }
 
 
 locals {
   azs              =  slice(data.aws_availability_zones.azs.names,0,var.k8s_az_count)
-  bastion_dns      = "bastion.${var.cluster_name}.${data.aws_route53_zone.hosted_zone.name}"
   addons_max_count =  length(local.azs)
 }
 data "aws_availability_zones" "azs" {
